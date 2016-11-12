fa4ad4031271cdae0970c44139d2149b8f48b7ee
Jay Kumar <jaykch@outlook.com>
2016-11-12 18:09:59 +0000

    Finalised App

--- index.js
+++ index.js
@@ -27,12 +27,13 @@ var userList = (function () {
             if (apiCall[i].display_name) {
                 log(apiCall[i].display_name);
                 url = "https://www.twitch.tv/" + apiCall[i].display_name;
-                imageUrl = "https://static-cdn.jtvnw.net/jtv_user_pictures/" + apiCall[i].display_name + "-profile_image-9021dccf9399929e-300x300.jpeg";
+                imageUrl = "https://static-cdn.jtvnw.net/jtv_user_pictures/" + apiCall[i].display_name.toLowerCase() + "-profile_image-9021dccf9399929e-300x300.jpeg";
+                log(imageUrl);
                 users.push(new user(apiCall[i].display_name, url, "offline", "offline", "active", imageUrl));
 
             } else if (apiCall[i].stream) {
                 log(apiCall[i].stream.display_name);
-                users.push(new user(apiCall[i].stream.display_name, apiCall[i].stream.url, "streaming", apiCall[i].stream.status, "active", apiCall[i].stream.logo));
+                users.push(new user(apiCall[i].stream.display_name, apiCall[i].stream.url, "streaming", apiCall[i].stream.status, "active", apiCall[i].stream.logo.toLowerCase()));
             } else {
                 log("Unknown User");
                 users.push(new user("User Doesn't Exist", "", "", "", "Account Doesn't Exist", ""));
@@ -44,21 +45,25 @@ var userList = (function () {
             if (users[i].accountStatus == "active") {
 
                 if (users[i].onlineStatus == "offline") {
-                    userOutput.innerHTML += "<li class='user-offline'>" + "<a class='user-name' href='"
-                        + users[i].url + "' target='_blank'>" + users[i].name + "</a>"
-                        + "<span class='online-status'>Status: " + users[i].onlineStatus + " </span>"
+                    userOutput.innerHTML += "<li class='user-offline'>"
+                        + "<img src='" + users[i].logo + "' alt='profile' class='user-logo'>"
+                        + "<a class='user-name' href='"
+                        + users[i].url + "' target='_blank'>" + users[i].name + "</a><br>"
+                        + "<span class='online-status'>Status: " + users[i].onlineStatus + " </span><br>"
                         + "<span class='stream-status'>Stream Status: " + users[i].streamStatus + "</span>"
                         + "</li>";
                 } else {
-                    userOutput.innerHTML += "<li class='user-online'>" + "<a class='user-name' href='"
-                        + users[i].url + "' target='_blank'>" + users[i].name + "</a>"
-                        + "<span class='online-status'>Status: " + users[i].onlineStatus + " </span>"
+                    userOutput.innerHTML += "<li class='user-online'>"
+                        + "<img src='" + users[i].logo + "' alt='profile' class='user-logo'>"
+                        + "<a class='user-name' href='"
+                        + users[i].url + "' target='_blank'>" + users[i].name + "</a><br>"
+                        + "<span class='online-status'>Status: " + users[i].onlineStatus + " </span><br>"
                         + "<span class='stream-status'>Stream Status: " + users[i].streamStatus + "</span>"
                         + "</li>";
                 }
             }
             else {
-                userOutput.innerHTML += "<li>" + "<span class='user-name'>" + users[i].name + " (404)</span></li>"
+                userOutput.innerHTML += "<li class='user-offline'>" + "<span class='user-name'>" + users[i].name + " (404)</span></li>"
             }
         }
     };
